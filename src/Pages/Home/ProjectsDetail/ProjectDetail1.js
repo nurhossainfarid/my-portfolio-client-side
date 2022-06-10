ec57fde4acdc0578a8e4b3dc72d241b32151721b
nurhossainfarid <faahsan1516@gmail.com>
2022-06-10 23:10:38 +0600

    add project details

--- src/Pages/Home/ProjectsDetail/ProjectDetail1.js
+++ src/Pages/Home/ProjectsDetail/ProjectDetail1.js
@@ -1,41 +1,32 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
-import ProjectPic1 from '../.../../../../images/project01.png'
+import ProjectPic1 from '../.../../../../images/firstProjectDemo.png'
 
 const ProjectDetail1 = () => {
     return (
         <div>
-            <h1>Welcome to project details 01</h1>
-            <Carousel>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src={ProjectPic1}
-                    alt="First slide"
-                    />
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src={ProjectPic1}
-                    alt="Second slide"
-                    />
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src={ProjectPic1}
-                    alt="Third slide"
-                    />
-                </Carousel.Item>
-            </Carousel>
-            <div class="card card-compact w-96 bg-base-100 shadow-xl">
-                <figure><img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" /></figure>
+            <div class="card card-compact w-3/4 mx-auto bg-base-100 shadow-xl">
+                <figure>
+                    <img src={ProjectPic1} alt="" />
+                </figure>
                 <div class="card-body">
-                    <h2 class="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
+                    <h2 class="card-title text-3xl">Project Name : <span className='text-primary text-4xl font-bold'>.carBro</span></h2>
+                    <h2 className="text-2xl font-semibold">Project Review : </h2>
+                    <ul className='list-disc ml-10 text-xl'>
+                        <li>This is the Car Parts Manufacturer website.</li>
+                        <li>In the login page, you want to be created a new account and you have an account then you login and in sign page you create a new user</li>
+                        <li>When you are valid user, you see dashboard. In dashboard, you see some nested route.</li>
+                    </ul>
+                    <div class="card-actions justify-start mt-5 gap-5">
+                    <div class="badge badge-outline text-primary badge-lg hover:scale-110 hover:bg-primary hover:text-white">
+                        <a  target="_blank" href="https://car-bro-4747.firebaseapp.com/">Live Demo</a>        
+                    </div>
+                    <div class="badge badge-outline text-secondary badge-lg hover:scale-110 hover:bg-secondary hover:text-white">
+                        <a  target="_blank" href="https://car-bro-4747.firebaseapp.com/">Client Side Code</a>        
+                    </div>
+                    <div class="badge badge-outline text-accent badge-lg hover:scale-110 hover:bg-accent hover:text-white">
+                        <a  target="_blank" href="https://car-bro-4747.firebaseapp.com/">Server Side Code</a>        
+                    </div>
                     </div>
                 </div>
             </div>
