54c33f38a11b20cf16a72f55bcc6243a548d19b3
nurhossainfarid <faahsan1516@gmail.com>
2022-06-11 01:11:04 +0600

    add animation

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import MyPic from '../../../images/mypic.png'
+import MyPic from '../../../images/mypic.png';
+import './Banner.css';
 
 const Banner = () => {
     return (
@@ -10,7 +11,7 @@ const Banner = () => {
                     <div className="text-center md:text-left md:text-start">
                     <h1 className="text-lg md:text-xl text-gray-400 uppercase">Welcome to my world</h1>
                     <h1 className="text-3xl md:text-5xl font-bold text-white mt-1 md:mt-2">Hi, I'm Nur Hossain Farid</h1>
-                    <h1 className="text-3xl md:text-5xl font-bold text-primary mt-2 md:mt-5">MERN STACK DEVELOPER</h1>
+                    <h1 className="text-3xl md:text-5xl font-bold text-primary mt-2 md:mt-5 animation" data-text="MernStackDeveloPer...">MernStackDeveloPer...</h1>
                         <p className="py-6 text-white text-sm md:text-lg">I am a MERN developer. I can provide clean code and pixel perfect design. I also make a dynamic website more & more interactive with web animations.</p>
                     <div className="flex gap-5 md:flex-row flex-col w-2/3 mx-auto md:w-full">
                         <button className="btn bg-inherit border-2 hover:bg-primary tex-md md:text-lg text-white hover:scale-110 hover:border-primary"><a target="_blank" href="https://drive.google.com/file/d/18Zk0ccjPUBqfvQAdg6IhBgngUQFxzW6S/view?fbclid=IwAR3EFSxrlq8YomNh7Uc46HIuWjHeEhYb7uFp1kv_zVe-lWZ2hEKXiyEdTz8">Download Resume</a></button>
