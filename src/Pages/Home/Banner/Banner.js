cf357224310b652b2a79591e1af52bccdaa34c20
nurhossainfarid <faahsan1516@gmail.com>
2022-06-13 13:17:42 +0600

    add particl ja

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,10 +1,97 @@
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 import React from 'react';
 import MyPic from '../../../images/mypic.png';
 import './Banner.css';
 
+
 const Banner = () => {
+    const particlesInit = async (main) => {
+        await loadFull(main);
+      };
+    
+      const particlesLoaded = (container) => {
+        // console.log(container);
+      };
     return (
         <div className="bg-black pb-10">
+                <Particles
+                    id="tsparticles"
+                    init={particlesInit}
+                    loaded={particlesLoaded}
+                    options={{
+                        background: {
+                        color: {
+                            value: "black",
+                        },
+                        },
+                        fpsLimit: 120,
+                        interactivity: {
+                        events: {
+                            onClick: {
+                            enable: true,
+                            mode: "push",
+                            },
+                            onHover: {
+                            enable: true,
+                            mode: "repulse",
+                            },
+                            resize: true,
+                        },
+                        modes: {
+                            push: {
+                            quantity: 4,
+                            },
+                            repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                            },
+                        },
+                        },
+                        particles: {
+                        color: {
+                            value: "#ffffff",
+                        },
+                        links: {
+                            color: "#ffffff",
+                            distance: 150,
+                            enable: true,
+                            opacity: 0.5,
+                            width: 1,
+                        },
+                        collisions: {
+                            enable: true,
+                        },
+                        move: {
+                            direction: "none",
+                            enable: true,
+                            outModes: {
+                            default: "bounce",
+                            },
+                            random: false,
+                            speed: 6,
+                            straight: false,
+                        },
+                        number: {
+                            density: {
+                            enable: true,
+                            area: 800,
+                            },
+                            value: 80,
+                        },
+                        opacity: {
+                            value: 0.5,
+                        },
+                        shape: {
+                            type: "circle",
+                        },
+                        size: {
+                            value: { min: 1, max: 5 },
+                        },
+                        },
+                        detectRetina: true,
+                    }}
+                    />
             <div className="hero bg-black ">
                 <div className="hero-content flex-col px-10 lg:flex-row-reverse p-0 gap-5 h-full">
                     <img src={MyPic} className="rounded-bl-full shadow-2xl border-b-4 border-primary" />
